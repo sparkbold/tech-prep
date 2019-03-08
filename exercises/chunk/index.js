066f81548679e45bec691e05f4aeca75c033b952
sparkbold-git <sparkboldstudio@gmail.com>
2019-03-08 12:48:18 -0600

    add algocast exercises

---
+++ exercises/chunk/index.js
@@ -0,0 +1,30 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+  //create chunkedArr
+  const chunked = [];
+  //loop through the arr and
+  for (let el of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([el]); // add el as an array element to chunked array
+    } else {
+      last.push(el);
+    }
+  }
+  return chunked;
+}
+
+module.exports = chunk;
+
+const arr = [1, 2, 3, 4, 5];
+console.log(chunk(arr, 2));
---
+++ exercises/reversestring/index.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {}
+
+module.exports = reverse;
+
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+// function reverse(str) {
+//   let rev = "";
+//   for (let char of str) {
+//     rev = char + rev;
+//   }
+//   return rev;
+// }

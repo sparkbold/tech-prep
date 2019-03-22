7fb15f694774677bd599b05bea7eaf0fac97ab73
sparkbold-git <sparkboldstudio@gmail.com>
2019-03-21 19:49:37 -0500

    change folder name

--- Node-js-Design-Patterns/Exercise Files/Ch02/02_05/start/scout_prototype.js
+++
@@ -1,9 +0,0 @@
-var Shopper = require('./Shopper');
-
-var scout = new Shopper();
-scout.addItemToList('camping knife');
-scout.addItemToList('tent');
-scout.addItemToList('backpack');
-scout.addItemToList('map');
-
-module.exports = scout;

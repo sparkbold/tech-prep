2f4a35fd5a50017e7b24a65ac8d619e0ef0ec275
sparkbold-git <sparkboldstudio@gmail.com>
2019-03-24 23:10:30 -0500

    hacker-rank

---
+++ hacker-rank/day1-data-types/index.js
@@ -0,0 +1,41 @@
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on("data", function(data) {
+  input_stdin += data;
+});
+
+process.stdin.on("end", function() {
+  input_stdin_array = input_stdin.split("\n");
+  main();
+});
+
+// Reads complete line from STDIN
+function readLine() {
+  return input_stdin_array[input_currentline++];
+}
+
+function main() {
+  var i = 4;
+  var d = 4.0;
+  var s = "HackerRank ";
+  // Declare second integer, double, and String variables.
+  let int = parseInt(input_stdin_array[0]),
+    dou = parseFloat(input_stdin_array[1]),
+    str = input_stdin_array[2];
+  // Read and save an integer, double, and String to your variables.
+  int = parseInt(i + int);
+  dou = parseFloat(d + dou).toFixed(1);
+  str = s + str;
+  // Print the sum of both integer variables on a new line.
+  console.log(int);
+  // Print the sum of the double variables on a new line.
+  console.log(dou);
+  // Concatenate and print the String variables on a new line
+  // The 's' variable above should be printed first.
+  console.log(str);
+}
